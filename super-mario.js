7f1123601addc96541fd19d08553bc76cbbd9335
baileyjordann11 <bailey.jordann11@gmail.com>
2026-02-14 12:08:07 -0600

    added left and right function

--- super-mario.js
+++ super-mario.js
@@ -32,11 +32,23 @@ function jump() {
 }
 
 function keyPressed() {
-  if (key === " ") jump();
+  if (key === "f") jump();
+  
+  if (key === "d") moveRight();
+  
+  if (key === "a") moveLeft();
 
 }
 
 
+
+function moveRight(){
+  x = x+5;
+}
+
+function moveLeft () { 
+x = x+5;
+}
 // ==================================================
 // 🧠 JUMP LOGIC
 // ==================================================
